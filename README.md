2beccafe5cfa3e0d925c4cc22a1e5df55e4e4eee
jiangzhenhao <18816784184@163.com>
2020-09-03 10:21:55 +0800

    初版消息队列（简化版本）

---
+++ README.md
@@ -0,0 +1,81 @@
+# mq
+消息队列
+
+## 开源地址
+https://github.com/JZhao1020/mq
+
+##1.安装
+```
+composer require hao/mq
+```
+
+##2.1 定义一个生产者（延迟消息）
+```
+$delay = new \mq\RabbitMQ();
+
+$ttl            = 1000 * 100;//订单100s后超时
+$delayExName    = 'delay-order-exchange';//超时exchange
+$delayQueueName = 'delay-order-queue';//超时queue
+$queueName      = 'ttl-order-queue';//订单queue
+
+$delay->createQueue($ttl, $delayExName, $delayQueueName, $queueName);
+$data = [
+    'order_no' => time(),
+    'remark'   => 'this is a order test'
+];
+$delay->sendMessage(json_encode($data), $queueName);
+```
+
+##2.2 定义一个消费者
+```
+$delay = new \mq\RabbitMQ();
+$delayQueueName = 'delay-order-queue';
+
+$callback = function ($msg) {
+    echo $msg->body . PHP_EOL;
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+
+    //处理订单超时逻辑，给用户推送提醒等等。。。
+    sleep(10);
+};
+
+/**
+ * 消费已经超时的订单信息，进行处理
+ */
+$delay->consumeMessage($delayQueueName, $callback);
+```
+
+##3.1 定义一个生产者
+```
+$delay = new \mq\RabbitMQ();
+$exName    = 'order-exchange';//exchange
+$queueName      = 'order-queue';//订单queue
+$delay->createExchange($exName, $queueName);
+
+$data = [
+    'order_no' => time(),
+    'remark'   => 'this is a order test'
+];
+$delay->sendMessage(json_encode($data), $queueName);
+  
+```
+
+##3.2 定义一个消费者
+```
+$delay = new \mq\RabbitMQ();
+$delayQueueName = 'order-queue';
+
+$callback = function ($msg) {
+    echo $msg->body . PHP_EOL;
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+
+    //处理订单超时逻辑，给用户推送提醒等等。。。
+    sleep(10);
+};
+
+/**
+ * 消费已经超时的订单信息，进行处理
+ */
+$delay->consumeMessage($delayQueueName, $callback);
+  
+```
---
+++ src/RabbitMQ.php
@@ -0,0 +1,121 @@
+<?php
+
+
+namespace mq;
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
+use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Wire\AMQPTable;
+
+class RabbitMQ
+{
+    public $config = [
+        'host' => '127.0.0.1', //ip
+        'port' => 5672,      //端口号
+        'user' => 'guest',     //用户
+        'password' => 'guest', //密码
+        'vhost' => '/'         //虚拟host
+    ];
+
+    protected $connection;
+    protected $channel;
+
+
+    /**
+     * RabbitMQ constructor.
+     */
+    public function __construct($config = [])
+    {
+        if($config) $this->setConfig($config);
+        $this->connection = new AMQPStreamConnection($this->config['host'], $this->config['port'], $this->config['user'], $this->config['password'], $this->config['vhost']);
+        $this->channel    = $this->connection->channel();
+    }
+
+    //重新设置MQ的链接配置
+    public function setConfig($config)
+    {
+        if (!is_array($config)) {
+            throw new \Exception('config不是一个数组');
+        }
+        foreach ($config as $key => $value) {
+            $this->config[$key] = $value;
+        }
+    }
+
+    /**
+     * 创建延时队列
+     * @param $ttl
+     * @param $delayExName
+     * @param $delayQueueName
+     * @param $queueName
+     */
+    public function createQueue($ttl, $delayExName, $delayQueueName, $queueName)
+    {
+        $args = new AMQPTable([
+            'x-dead-letter-exchange'    => $delayExName,
+            'x-message-ttl'             => $ttl, //消息存活时间
+            'x-dead-letter-routing-key' => $queueName
+        ]);
+        $this->channel->queue_declare($queueName, false, true, false, false, false, $args);
+        //绑定死信queue
+        $this->channel->exchange_declare($delayExName, AMQPExchangeType::DIRECT, false, true, false);
+        $this->channel->queue_declare($delayQueueName, false, true, false, false);
+        $this->channel->queue_bind($delayQueueName, $delayExName, $queueName, false);
+    }
+
+    /**
+     * 创建实时队列
+     * @param $exchangeName
+     * @param $queueName
+     */
+    public function createExchange($exchangeName, $queueName)
+    {
+        //创建交换机$channel->exchange_declare($exhcange_name,$type,$passive,$durable,$auto_delete);
+        //passive: 消极处理， 判断是否存在队列，存在则返回，不存在直接抛出 PhpAmqpLib\Exception\AMQPProtocolChannelException 异常
+        //durable：true、false true：服务器重启会保留下来Exchange。警告：仅设置此选项，不代表消息持久化。即不保证重启后消息还在
+        //autoDelete:true、false.true:当已经没有消费者时，服务器是否可以删除该Exchange
+        $this->channel->exchange_declare($exchangeName, 'direct', false, true, false);
+        //passive: 消极处理， 判断是否存在队列，存在则返回，不存在直接抛出 PhpAmqpLib\Exception\AMQPProtocolChannelException 异常
+        //durable：true、false true：在服务器重启时，能够存活
+        //exclusive ：是否为当前连接的专用队列，在连接断开后，会自动删除该队列
+        //autodelete：当没有任何消费者使用时，自动删除该队列
+        //arguments: 自定义规则
+        $this->channel->queue_declare($queueName, false, true, false, false, false);
+    }
+
+    /**
+     * 生成信息
+     * @param $message
+     */
+    public function sendMessage($message, $routeKey, $exchange = '', $properties = [])
+    {
+        $data = new AMQPMessage(
+            $message, $properties
+        );
+        $this->channel->basic_publish($data, $exchange, $routeKey);
+    }
+
+    /**
+     * 消费消息
+     * @param $queueName
+     * @param $callback
+     * @throws \ErrorException
+     */
+    public function consumeMessage($queueName,$callback)
+    {
+        $this->channel->basic_consume($queueName, '', false, false, false, false, $callback);
+        while ($this->channel->is_consuming()) {
+            $this->channel->wait();
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function __destruct()
+    {
+        $this->channel->close();
+        $this->connection->close();
+    }
+}
